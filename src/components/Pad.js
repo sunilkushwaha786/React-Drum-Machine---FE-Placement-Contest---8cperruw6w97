921180cab4a6639a3c2cea2b75476a2acf619619
sunilkushwaha <sunilkushwaha055>
2023-03-26 16:56:30 +0530

    doneee

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -2,15 +2,28 @@ import React from "react";
 import {bank1} from "./App"
 
 function Pad({handleClick, power, backgroundStyle, element, id }) {
+  const audio = new Audio (bank1[element].source)
+
+  window.addEventListener("keydown", (event) =>{
+    if (event.key === element || event.key === element.toLowerCase()){
+      handleClick (bank1[element]);
+      audio.play();
+    }
+  });
+
     return (
       <button 
         data-tag={id}
         type='button'
         className='drum-pad' 
-        onClick={handleClick} 
+        onClick={()=>{
+        audio.play();
+        handleClick(bank1[element]);
+      }}
         id={bank1[element]} 
         disabled={!power} 
-        style={{background: `${backgroundStyle}`}}>
+        style={{background: `${backgroundStyle}`}}
+        >
         {element}
         <audio id={element} src={bank1[element].source} className='clip'></audio>
       </button>
